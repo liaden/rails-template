844e02a5a7f0f8d874ba805d2b72683fef194716
Joel Johnson <jjohnson10@enova.com>
2020-01-18 14:30:18 -0600

    Serve stylesheets via webpack

--- app/template.rb
+++ app/template.rb
@@ -1,5 +1,5 @@
-copy_file "app/assets/stylesheets/application.scss"
-remove_file "app/assets/stylesheets/application.css"
+copy_file "app/javascript/stylesheets/application.scss"
+remove_file "app/javascript/stylesheets/application.css"
 
 copy_file "app/controllers/home_controller.rb"
 copy_file "app/helpers/layout_helper.rb"
