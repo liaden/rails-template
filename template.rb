752cdf5fc158933c0afc1e268f617cc408de6a8d
Joel Johnson <jjohnson10@enova.com>
2020-01-18 13:42:29 -0600

    Point to my fork

--- template.rb
+++ template.rb
@@ -75,7 +75,7 @@ def add_template_repository_to_source_path
     at_exit { FileUtils.remove_entry(tempdir) }
     git clone: [
       "--quiet",
-      "https://github.com/mattbrictson/rails-template.git",
+      "https://github.com/liaden/rails-template.git",
       tempdir
     ].map(&:shellescape).join(" ")
 
